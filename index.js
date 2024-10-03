dcb4df7ddb7d2ef5ae33beb28db6840dc7da43d1
Margit Viigi <margit.viigi@voco.ee>
2024-10-03 14:38:11 +0300

    Create project basic files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.get('/', (req, res)=>{
+    res.render
+    res.send ('test nodemon')
+})
+app.listen(3001, () =>{
+    console.log('Server is started.')
+} )
