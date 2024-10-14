0c1231320a8060d31bbfd7f501bac4bcc3aba1b0
Margit Viigi <margit.viigi@voco.ee>
2024-10-14 14:11:07 +0300

    Salvestan

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express()
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res)=>{
-    res.render('index')
+    const tasks = ['Study HTML','Study CSS', 'Study JS'] 
+    
+    res.render('index',{tasks: tasks} )
 })
 app.listen(3001, () =>{
-    console.log('Server is started.')
+    console.log('Server is started http://http://localhost:3001')
 } )
