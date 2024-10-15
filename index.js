4f1e97a4bc282748f9ae8a9c2a949832647251b7
Margit Viigi <margit.viigi@voco.ee>
2024-10-15 10:10:33 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const path = require('path')
+const fs = require('node:fs');
 const app = express()
+
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.get('/', (req, res)=>{
-    const tasks = ['Study HTML','Study CSS', 'Study JS'] 
+app.get('/', (req, res)=> {
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+    const tasks = data.split('\n')
     
     res.render('index',{tasks: tasks} )
+ });
 })
-app.listen(3001, () =>{
-    console.log('Server is started http://http://localhost:3001')
+
+app.listen(3001, () => {
+    console.log('Server is started http://localhost:3001')
 } )
+
+
+
