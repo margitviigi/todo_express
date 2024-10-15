9cf837246b9ffe4120f36a8f7453a2d54cc27e4f
Margit Viigi <margit.viigi@voco.ee>
2024-10-15 11:46:57 +0300

    Andmete salvestamine faili

--- index.js
+++ index.js
@@ -3,20 +3,52 @@ const path = require('path')
 const fs = require('node:fs');
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+              const tasks = data.split('\n') 
+              resolve(tasks)
+           });
+    })
+} 
+
 app.get('/', (req, res)=> {
-    fs.readFile('./tasks', 'utf8', (err, data) => {
+    readFile('./tasks')
+    .then((tasks) => {
+        res.render('index',{tasks: tasks})
+    })
+
+    
+    
+})
+app.post('/', (req, res) =>{
+    console.log('form sent data')
+    let task = req.body.task
+    readFile('./tasks')
+    .then((tasks) => {
+        tasks.push(task)
+        console.log(tasks)
+        const data = tasks.join('\n')
+        const fs = require('node:fs');
+
+fs.writeFile('./tasks', data, err => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+   res.redirect('/')
   }
-    const tasks = data.split('\n')
-    
-    res.render('index',{tasks: tasks} )
- });
-})
-
+});
+    })
+       });
 app.listen(3001, () => {
     console.log('Server is started http://localhost:3001')
 } )
